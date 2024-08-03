d01c13f49aac21d628a7097969781fefcfb4c7ad
mcneds <aidenkuemmerle@gmail.com>
2024-08-02 22:59:59 -0700

    test commit

--- src/main/java/dev/mcneds/mccoursemod/McCourseMod.java
+++ src/main/java/dev/mcneds/mccoursemod/McCourseMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//comment sdf
 public class McCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mc-course-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
