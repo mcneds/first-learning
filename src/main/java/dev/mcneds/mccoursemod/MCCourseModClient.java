130fc500f049b61f12e5233a4d2fc7e65d0bfae9
mcneds <aidenkuemmerle@gmail.com>
2024-08-02 22:52:45 -0700

    Initial commit

---
+++ src/main/java/dev/mcneds/mccoursemod/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package dev.mcneds.mccoursemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/dev/mcneds/mccoursemod/McCourseModDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.mcneds.mccoursemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class McCourseModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
